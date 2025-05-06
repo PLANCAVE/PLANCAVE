a6a2108f7a382711285553c68570cc90a169f1d8
clifford manase <96686740+Cliffy21@users.noreply.github.com>
2025-05-06 16:45:32 +0300

    Delete Categories.js

--- Categories.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String },
-});
-categorySchema.index({ name: 1 }); // Index category name
-
-module.exports = mongoose.model('Category', categorySchema);

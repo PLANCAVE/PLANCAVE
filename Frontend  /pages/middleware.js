4ddd1f28cf11c72a8199b393a8efa31ad5073361
clifford manase <96686740+Cliffy21@users.noreply.github.com>
2025-05-06 18:13:18 +0300

    Add files via upload

---
+++ Frontend  /pages/middleware.js
@@ -0,0 +1,11 @@
+/*import { withClerkMiddleware } from '@clerk/nextjs';
+
+export default withClerkMiddleware({
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+});
+
+// Optional matcher
+export const config = {
+  matcher: ['/((?!_next).*)', '/', '/api/(.*)'],
+};
+console.log('Publishable Key:', process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);*/

4168a535db7f1963a55240f6ec06eefeff102dd1
Cycus pectus <46060216+cy-cus@users.noreply.github.com>
2025-06-04 11:17:26 +0300

    Delete plancave/auth_api directory

--- plancave/auth_api/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-default')
-    DATABASE_URL = os.getenv('DATABASE_URL')
-

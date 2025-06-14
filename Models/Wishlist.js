ec9a89b5deedd697d51805fc4c2cfc2cc35e7790
clifford manase <96686740+Cliffy21@users.noreply.github.com>
2025-06-14 18:28:21 +0300

    Delete Models directory

--- Models/Wishlist.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const wishlistSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to User schema
-  plans: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Plan' }], // Array of Plan references
-}, { timestamps: true });
-//wishlistSchema.index({ userId: 1 }); // Index userId to find user-specific wishlist
-
-module.exports = mongoose.model('Wishlist', wishlistSchema);
